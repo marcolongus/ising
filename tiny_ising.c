4b00cea63d2e1ac3ce8e48ff8130572ac541f39b
marcolongus <benjaminmarcolongo@gmail.com>
2021-05-26 20:24:58 -0300

    version 1

--- tiny_ising.c
+++ tiny_ising.c
@@ -58,17 +58,19 @@ static void cycle(int grid[L][L],
         // measurement phase
         unsigned int measurements = 0;
         double e = 0.0, e2 = 0.0, e4 = 0.0, m = 0.0, m2 = 0.0, m4 = 0.0;
+        
         for (unsigned int j = 0; j < TMAX; ++j) {
+            
             update(temp, grid);
             if (j % calc_step == 0) {
                 double energy = 0.0, mag = 0.0;
                 int M_max = 0;
                 energy = calculate(grid, &M_max);
                 mag = abs(M_max) / (float)N;
-                e += energy;
+                e  += energy;
                 e2 += energy * energy;
                 e4 += energy * energy * energy * energy;
-                m += mag;
+                m  += mag;
                 m2 += mag * mag;
                 m4 += mag * mag * mag * mag;
                 ++measurements;
@@ -97,6 +99,7 @@ static void init(int grid[L][L])
 }
 
 
+
 int main(void)
 {
     // parameter checking
@@ -113,18 +116,42 @@ int main(void)
         stat[i].m = stat[i].m2 = stat[i].m4 = 0.0;
     }
 
+
+    // files
+    FILE * data;
+    FILE * ejec_time;
+    data      = fopen("data_nuevo.txt"     , "w");
+    ejec_time = fopen("ejec_time.txt"      , "a");
+
     // print header
-    printf("# L: %i\n", L);
-    printf("# Minimum Temperature: %f\n", TEMP_INITIAL);
-    printf("# Maximum Temperature: %f\n", TEMP_FINAL);
-    printf("# Temperature Step: %.12f\n", TEMP_DELTA);
-    printf("# Equilibration Time: %i\n", TRAN);
-    printf("# Measurement Time: %i\n", TMAX);
-    printf("# Data Acquiring Step: %i\n", DELTA_T);
-    printf("# Number of Points: %i\n", NPOINTS);
+    printf("# L                  : %i\n", L)           ;
+    printf("# Minimum Temperature: %f\n", TEMP_INITIAL );
+    printf("# Maximum Temperature: %f\n", TEMP_FINAL   );
+    printf("# Temperature Step   : %.12f\n", TEMP_DELTA);
+    printf("# Equilibration Time : %i\n", TRAN         );
+    printf("# Measurement Time   : %i\n", TMAX         );
+    printf("# Data Acquiring Step: %i\n", DELTA_T      );
+    printf("# Number of Points   : %i\n", NPOINTS      );
+
+    unsigned int impri=1;
+
+    if (impri==0){
+        fprintf(ejec_time,"#gcc -std=c11 -Wall -Wextra -O1 afuera spin update y calculate\n");
+        fprintf(ejec_time,"# L                  : %i\n", L            );
+        fprintf(ejec_time,"# Minimum Temperature: %f\n", TEMP_INITIAL );
+        fprintf(ejec_time,"# Maximum Temperature: %f\n", TEMP_FINAL   );
+        fprintf(ejec_time,"# Temperature Step   : %.12f\n", TEMP_DELTA);
+        fprintf(ejec_time,"# Equilibration Time : %i\n", TRAN         );
+        fprintf(ejec_time,"# Measurement Time   : %i\n", TMAX         );
+        fprintf(ejec_time,"# Data Acquiring Step: %i\n", DELTA_T      );
+        fprintf(ejec_time,"# Number of Points   : %i\n", NPOINTS      );
+    }
+
 
     // configure RNG
     srand(SEED);
+    // MTRand r = seedRand(1337);
+    
 
     // start timer
     double start = wtime();
@@ -138,11 +165,13 @@ int main(void)
 
     // stop timer
     double elapsed = wtime() - start;
+
+    fprintf(ejec_time,"%lf\n", 4500*((double)L*(double)L)/(1000000*elapsed));
     printf("# Total Simulation Time (sec): %lf\n", elapsed);
 
-    printf("# Temp\tE\tE^2\tE^4\tM\tM^2\tM^4\n");
+    fprintf(data,"# Temp\t\tE\t\t\t\tE^2\t\t\t\tE^4\t\t\t\tM\t\t\t\tM^2\t\t\t\tM^4\n");
     for (unsigned int i = 0; i < NPOINTS; ++i) {
-        printf("%lf\t%.10lf\t%.10lf\t%.10lf\t%.10lf\t%.10lf\t%.10lf\n",
+        fprintf(data,"%lf\t%.10lf\t%.10lf\t%.10lf\t%.10lf\t%.10lf\t%.10lf\n",
                stat[i].t,
                stat[i].e / ((double)N),
                stat[i].e2 / ((double)N * N),
@@ -151,6 +180,7 @@ int main(void)
                stat[i].m2,
                stat[i].m4);
     }
-
+    fclose(data);
+    fclose(ejec_time);
     return 0;
 }
